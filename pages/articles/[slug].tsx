206e4c905b55f296c352a83d182a07a0abb4cd58
Bohdan <ghbdtnkfkrf@gmail.com>
2025-07-27 12:58:55 +0300

    feat: upload sport-archive v3

--- pages/articles/[slug].tsx
+++ pages/articles/[slug].tsx
@@ -1,12 +1,1 @@
-import { useRouter } from "next/router";
-
-export default function ArticlePage() {
-  const { slug } = useRouter().query;
-
-  return (
-    <main style={{ padding: "2rem", fontFamily: "Manrope" }}>
-      <h1>Упражнение: {slug}</h1>
-      <p>Контент упражнения появится здесь...</p>
-    </main>
-  );
-}
+// Страница отдельной статьи упражнения
